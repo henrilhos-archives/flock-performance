42692a9d30060b107a5bb020378d92632c5309bb
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-04-02 11:25:48 -0300

    feat: add a chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum Colors {
+  PURPLE = "#7E5B99",
+  AQUA = "#018787",
+  BROWN = "#744730",
+  PINK = "#CB4777",
+  ORANGE = "#FF6000",
+  GREEN = "#9CD70F",
 }
+
+enum Symbols {
+  CIRCLE = "circle",
+  SQUARE = "square",
+  DIAMOND = "diamond",
+  TRIANGLE = "triangle",
+  TRIANGLE_DOWN = "triangle-down",
+}
+
+const options: Highcharts.Options = {
+  title: { text: "Flock performance over time" },
+  legend: {
+    layout: "vertical",
+    align: "left",
+    verticalAlign: "top",
+  },
+  navigation: { buttonOptions: { enabled: false } },
+  xAxis: {
+    title: { text: "Age (weeks)" },
+    crosshair: true,
+  },
+  yAxis: [
+    { title: { text: undefined }, labels: { format: "{value}%" } },
+    { title: { text: undefined }, opposite: true },
+  ],
+  plotOptions: {
+    series: {
+      selected: true,
+      showCheckbox: true,
+      marker: { enabled: false },
+      label: { enabled: false },
+    },
+  },
+  series: [
+    {
+      name: "Cum. total mortality (%)",
+      type: "line",
+      yAxis: 0,
+      tooltip: { valueSuffix: "%" },
+      color: Colors.PURPLE,
+      marker: { symbol: Symbols.CIRCLE },
+      data: [1, 2, 3],
+    },
+    {
+      name: "Cum. total mortality goal (%)",
+      dashStyle: "Dash",
+      type: "line",
+      yAxis: 0,
+      tooltip: { valueSuffix: "%" },
+      color: Colors.PURPLE,
+      marker: { symbol: Symbols.CIRCLE },
+      data: [3, 2, 1],
+    },
+    {
+      name: "Hen-day (%)",
+      type: "line",
+      yAxis: 0,
+      tooltip: { valueSuffix: "%" },
+      color: Colors.AQUA,
+      marker: { symbol: Symbols.SQUARE },
+      data: [2, 3, 1],
+    },
+  ],
+};
+
+const App = (): JSX.Element => (
+  <div>
+    <HighchartsReact highcharts={Highcharts} options={options} />
+  </div>
+);
 
 export default App;
